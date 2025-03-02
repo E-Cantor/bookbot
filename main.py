d52e346d779b3d252df3d1b024b180c432e39b09
E-Cantor <elicantor1993@gmail.com>
2025-03-01 19:15:02 -0500

    Post project code annotation for clarity on fuction of functions.

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from stats import get_book_num_words, num_letters, sorting_letter
 import sys
 
+#Function to get the book path from *books/*.txt
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-        
+
+# Function for error handling in Python in the event that argv does not have 2 arguments.      
 def main():
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-    
+
+# Try/Except block for main fucntion to run with proper formatting of the results.
+# Exception block made to catch if the argv book path is invalid.
     path = sys.argv[1]
     try:
         text = get_book_text(path)
--- stats.py
+++ stats.py
@@ -1,8 +1,11 @@
+# Function to get the number of words in the books
 def get_book_num_words(text):
     words_in_book = text.split()
     num_words = len(words_in_book)
     return num_words
 
+# Function to get the number of letters in the books.
+# Only displays alpha characters.
 def num_letters(text):
     letter_count = {}
     letters = text.lower()
@@ -10,6 +13,7 @@ def num_letters(text):
         letter_count[letter] = letter_count.get(letter, 0) + 1
     return letter_count
 
+# Function to sort the letters into dictionaires dpending on frequency in the book.
 def sorting_letter(letter_count):
     # Filter for alphabetic characters and convert to list of tuples
     alpha_count = [(letter, count) for letter, count in letter_count.items() if letter.isalpha()]
